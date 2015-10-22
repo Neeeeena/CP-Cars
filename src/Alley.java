c5296b7226c304693a845d4b7c2321b92b6cde71
bisserup15 <bisserup.panda@gmail.com>
2015-10-22 15:33:31 +0200

    Move: Alley class into its own file

--- src/Alley.java
+++ src/Alley.java
@@ -1,11 +1,33 @@
-
-public class Alley {
+public class Alley{
+	Semaphore sAlley = new Semaphore(1);
+	int clockWise = 0;
+	int counterWise = 0;
 	
-	public void enter(int no){
-		
-	} 
+	public void enter(int no) throws InterruptedException{
+		if(no == 1 || no == 2 || no == 3 || no == 4){
+			if(clockWise == 0){
+				sAlley.P();
+				clockWise++;
+			} else clockWise ++;
+		} else{
+			if(counterWise == 0){
+				sAlley.P();
+				counterWise++;
+			} else counterWise ++;
+		}
+	}
 
-	public void leave(int no){
-		
+	public void leave(int no) throws InterruptedException{
+		if(no == 1 || no == 2 || no == 3 || no == 4){
+			if(clockWise == 1){
+				sAlley.V();
+				clockWise--;
+			} else clockWise --;
+		} else{
+			if(counterWise == 1){
+				sAlley.V();
+				counterWise--;
+			} else counterWise --;
+		}
 	}
 }
