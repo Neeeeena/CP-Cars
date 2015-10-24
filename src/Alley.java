cc6eb0cc534923fcceadd46c78dd1c90dfe46fd2
Neeeeena <Nina@192.168.0.163>
2015-10-24 13:41:35 +0200

    Jeg har tilføjet en first semaphore for counterclockwise cars også.

--- src/Alley.java
+++ src/Alley.java
@@ -2,28 +2,30 @@ public class Alley{
 	Semaphore sAlley = new Semaphore(1);
 	int clockWise = 0;
 	int counterWise = 0;
-	Semaphore first = new Semaphore(1);
+	Semaphore firstc = new Semaphore(1);
+	Semaphore firstcc = new Semaphore(1);
 
 	
 	
 	public void enter(int no) throws InterruptedException{
 		// 0 = clockwise
 		if(no == 0){
-			first.P();
+			firstc.P();
 			if(clockWise == 0){
 				sAlley.P();
 				clockWise++;
 			} else clockWise ++; 
-			first.V();
+			firstc.V();
 		} else{
+			firstcc.P();
 			if(counterWise == 0){
 				sAlley.P();
 				counterWise++;
 			} else counterWise ++;
+			firstcc.V();
 		}
 	}
 	
-
 	public void leave(int no) throws InterruptedException{
 		if(no == 0){
 			if(clockWise == 1){
